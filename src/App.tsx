05de4c8ed948152b4339a8237a2f4022d082c11b
wujuno <huj88854@gmail.com>
2023-04-06 16:54:52 +0900

    Feat: 회원가입 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./Signup";
+import SignIn from "./Signin";
+import Home from "./Home";
+
 function App() {
-  return <div></div>;
+  const isLogin = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
